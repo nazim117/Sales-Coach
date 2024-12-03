b1af283e75eb850e2ffa52bb45abb56340429ed4
Nazim <nahmedov347@gmail.com>
2024-12-03 20:58:00 +0100

    NLP added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 import whisper
+from textblob import TextBlob
+import nltk
+from dotenv import load_dotenv
+import os
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
+
+load_dotenv()
 
 model = whisper.load_model("base")
 
@@ -6,6 +17,17 @@ def transcribe_and_translate(audio_path):
     result = model.transcribe(audio_path, task="translate")
     return result["text"]
 
-audio_file = r"C:\Users\nahme\Downloads\test-audio-file.mp3"
+
+audio_file = os.getenv('AUDIO_URL')
 translated_text = transcribe_and_translate(audio_file)
+
 print("Translated Transcription:", translated_text)
+
+blob = TextBlob(translated_text)
+
+print("POS Tags:", blob.tags)
+
+print("Noun Phrases:", blob.noun_phrases)
+
+for sentence in blob.sentences:
+    print("Sentence Sentiment Polarity:", sentence.sentiment.polarity)
