2b6e89282e31434083b35ed805ac13d85e058d99
Nazim <nahmedov347@gmail.com>
2024-12-04 15:43:47 +0100

    data loading and cleaning

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,25 @@
 import pandas as pd
 
 def load_movie_lines(file_path):
-    lines = pd.read_csv(file_path, sep=' +++$+++ ', header=None, names=['line_id', 'character_id', 'movie_id', 'text'], engine='python')
+    lines = pd.read_csv(
+        file_path,
+        sep=r'\s\+\+\+\$\+\+\+\s',
+        header=None,
+        names=['line_id', 'character_id', 'movie_id', 'text'],
+        engine='python',
+        encoding='ISO-8859-1'
+    )
     return lines
 
 def load_movie_conversations(file_path):
-    conversations = pd.read_csv(file_path, sep=' +++$+++ ', header=None, names=['character_1', 'character_2', 'movie_id', 'lines'], engine='python')
+    conversations = pd.read_csv(
+        file_path,
+        sep=r'\s\+\+\+\$\+\+\+\s',
+        header=None,
+        names=['character_1', 'character_2', 'movie_id', 'lines'],
+        engine='python',
+        encoding='ISO-8859-1'
+    )
     return conversations
 
 def prepare_data(lines_file_path, conv_file_path):
--- src/main.py
+++ src/main.py
@@ -2,31 +2,26 @@ from transcription.transcriber import transcribe_and_translate
 from sentiment_analysis.analyzer import generate_summary_and_suggestions
 import os
 from dotenv import load_dotenv
+from data_loader import prepare_data
 
-load_dotenv()
+# load_dotenv()
 
-audio_file = os.getenv('AUDIO_URL')
+# audio_file = os.getenv('AUDIO_URL')
 
-translated_text = transcribe_and_translate(audio_file)
-print("Translated Transcription:", translated_text)
+# translated_text = transcribe_and_translate(audio_file)
+# print("Translated Transcription:", translated_text)
 
-result = generate_summary_and_suggestions(translated_text)
-print(result)
+# result = generate_summary_and_suggestions(translated_text)
+# print(result)
 
-from src.data_loader import prepare_data
-
-# For loading and processing data
-# def main():
-#     # Path to the data files
-#     lines_file_path = 'data/cornell_movie_dialogs/movie_lines.txt'
-#     conv_file_path = 'data/cornell_movie_dialogs/movie_conversations.txt'
+def main():
+    lines_file_path = 'data/archive/movie_lines.txt'
+    conv_file_path = 'data/archive/movie_conversations.txt'
     
-#     # Prepare data using the function from data_loader.py
-#     lines_df, conversations_df = prepare_data(lines_file_path, conv_file_path)
+    lines_df, conversations_df = prepare_data(lines_file_path, conv_file_path)
 
-#     # Print the first few rows of data for inspection
-#     print(lines_df.head())
-#     print(conversations_df.head())
+    print(lines_df.head())
+    print(conversations_df.head())
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
